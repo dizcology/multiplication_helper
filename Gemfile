bad090b8f86f1a0c3b73b25f8efd62f145f57d65
Matthew Short <mshort@happyfuncorp.com>
2014-08-30 08:17:33 -0400

    adds prettiness, header, footer, bootstrap rails, seeds, fixed migration

--- Gemfile
+++ Gemfile
@@ -37,3 +37,15 @@ gem 'sdoc', '~> 0.4.0',          group: :doc
 
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
 gem 'tzinfo-data', platforms: [:mingw, :mswin]
+
+
+# #####################
+# Added by Matt
+
+gem 'bootstrap-sass'
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'annotate' 
+end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,36 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+### Add some users.  I'm using an array of hashes that contain the user info
+
+[
+  {
+    name: "Matthew"
+  },
+  {
+    name: "Yu-Han"
+  },
+  {
+    name: "Candace"
+  }
+].each do |user|
+  User.find_or_create_by(name: user[:name])
+end
+
+### Add some items, same structure as before.  
+
+[
+  {
+    name: "Multiply",
+    content: "What is 5x3?"
+  },
+  {
+    name: "Add",
+    content: "What is 5+3?"
+  }
+].each do |item|
+  Item.find_or_create_by(name: item[:name], content: item[:content])
+end
+
+### You could do Item.find_or_create_by(name: "Multiply", content: "What is 5x3?"), Item.find_or_create_by(name...), and have one line for each thing, rather than pulling them from an array of hashes, but this feals more programattically correct to me.
