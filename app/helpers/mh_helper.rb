9e38a52817ce143a08cc4d91f1bf66d22e0b93e4
Matthew Short <mshort@happyfuncorp.com>
2014-08-30 20:20:35 -0400

    stuff

--- app/helpers/mh_helper.rb
+++ app/helpers/mh_helper.rb
@@ -1,16 +1,17 @@
 module MhHelper
-
-  @@replace={
+  @@replace = {
     "times" => "&times;"
-    }
+  }
 
   def parse(str)
-    temp=str
+    temp = str
+    
     @@replace.keys.each do |k|
       next unless temp.match(k)
-      temp.gsub!(k,@@replace[k])
-
+      
+      temp.gsub!(k, @@replace[k])
     end
+    
     return temp.html_safe
   end
 end
