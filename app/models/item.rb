8231344412bfebc2daa3cb54b2b72a1d70565e5f
dizcology <dizcology@hotmail.com>
2014-09-04 00:16:33 -0500

    added items and traits methods

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,4 +1,9 @@
 class Item < ActiveRecord::Base
   has_many :events
   has_many :records, through: :events
+  
+  def traits
+    # item traits are recorded in the tag string in the form "trait1|trait2|..."
+    Trait.where(name: self.name.split("|"))
+  end
 end
--- app/models/trait.rb
+++ app/models/trait.rb
@@ -2,4 +2,9 @@ class Trait < ActiveRecord::Base
   has_many :user_traits
   has_many :events
   has_many :records
+  
+  def items
+    nm = self.name
+    Item.where("tag = \"#{nm}\" OR tag LIKE \"%#{nm}|%\" OR tag LIKE \"%|#{nm}%\"")
+  end
 end
